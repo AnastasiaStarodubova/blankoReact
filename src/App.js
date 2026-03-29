fe2b7a9f502011b9ad8f154082a5f10d1774fd11
ili <dezergeometrydash2010@gmail.com>
2026-03-29 12:30:02 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import styled from "@emotion/styled";
-import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { Landing } from "./components/Landing";
 import { Herro } from "./components/Herro";
